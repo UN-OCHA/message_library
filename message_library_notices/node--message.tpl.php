2d67a9b7a062e4fa73a42293907ef01094c0735b
Kurt Zhong <kurt@wiredcraft.com>
2012-09-11 13:05:50 +0800

    Begin to emulate the node theme.

--- message_library_notices/node--message.tpl.php
+++ message_library_notices/node--message.tpl.php
@@ -4,6 +4,7 @@
  * Custom theme implementation to display a node of type - 'Message'
  *
  * The origial template has:
+ * - An anchor with node id
  * - A class 'message_highlight' on the outter div to mark this message as a
  *   search highlighted message
  * - Provide a edit link if the user has access for it
@@ -11,35 +12,31 @@
  * - Search highlight links
  */
 ?>
+<a name="<?php print $node->nid ?>"></a>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <?php print $user_picture; ?>
-
-  <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_prefix); ?>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+    <?php print render($title_suffix); ?>
 
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
+    <div class="clearfix">
+      <?php print $user_picture; ?>
+
+      <?php if ($ml_sensitivity_questions): ?>
+        <?php print $ml_sensitivity_questions; ?>
+      <?php endif; ?>
+
+
+      <div class="content"<?php print $content_attributes; ?>>
+        <?php
+          hide($content['links']);
+          print render($content);
+        ?>
+      </div>
+
     </div>
+
+    <?php print render($content['links']); ?>
+
   <?php endif; ?>
-
-  <?php if ($ml_sensitivity_questions): ?>
-    <?php print $ml_sensitivity_questions; ?>
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-
-  <?php print render($content['links']); ?>
-  <?php print render($content['comments']); ?>
 </div>
