04cac8738ad6ffa7e65018ac4f6475db5e341d3a
Kurt Zhong <kurt@wiredcraft.com>
2012-09-12 17:57:40 +0800

    Update the features

--- export2rtf/js/export2rtf.js
+++ export2rtf/js/export2rtf.js
@@ -1,17 +1,16 @@
 (function ($) {
-
   Drupal.behaviors.export2rtf = {
-      attach: function(context, settings) {
-    if ($('.view-message-library').size()>0){
-      if ($('.view-message-library #search_list_threat').size()>0) {
-        $('.block-export2rtf').show();
-        $('.export2rtf_link').attr("href",'/export2rtf/'+$("#search_list_threat").val());
-      }
-      else {
-        $('.block-export2rtf').hide();
+    attach: function(context, settings) {
+      if ($('.view-message-library').size()>0){
+        if ($('.view-message-library #search_list_threat').size()>0) {
+          $('.block-export2rtf').show();
+          $('.export2rtf_link').attr("href",'/export2rtf/'+$("#search_list_threat").val());
+        }
+        else {
+          $('.block-export2rtf').hide();
+        }
       }
     }
-      }
   };
 // END jQuery
 })(jQuery);
