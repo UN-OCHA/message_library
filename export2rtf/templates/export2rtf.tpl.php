a77c6db9763cc07ba3ed10e6b53a7d7b4d6b6584
Kurt Zhong <kurt@wiredcraft.com>
2012-10-18 15:06:02 +0800

    Finish rtf

--- export2rtf/templates/export2rtf.tpl.php
+++
@@ -1,5 +0,0 @@
-<div id="export2rtf_link" class="export2rtf_box">
-  <a href="<?php print export2rtf_get_url(); ?>" class="export2rtf_link">
-    <?php print t('Download as an RTF Document'); ?>
-  </a>
-</div>
