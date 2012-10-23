fbccfc72010e2f0591379e31728c87fb0c4d9bcd
Kurt Zhong <kurt@wiredcraft.com>
2012-10-23 20:49:25 +0800

    Some small fix

--- message_library_highlight/templates/search_highlight_links.tpl.php
+++ message_library_highlight/templates/search_highlight_links.tpl.php
@@ -1,9 +1,8 @@
 <div class="search_highlight_links">
-<?php if ($prev_link != '') : ?>
-  <a href="<?php echo $prev_link; ?>" class="search_highlight_links_previous"><?php echo t("Previous highlight message"); ?></a>
-<?php endif; ?>
-<?php if ($next_link != '') : ?>
-<a href="<?php echo $next_link; ?>" class="search_highlight_links_next"><?php echo t("Next highlight message"); ?></a>
-<?php endif; ?>
+  <?php if ($prev_link != '') : ?>
+    <a href="<?php echo $prev_link; ?>" class="search_highlight_links_previous"><?php echo t("Previous highlight message"); ?></a>
+  <?php endif; ?>
+  <?php if ($next_link != '') : ?>
+    <a href="<?php echo $next_link; ?>" class="search_highlight_links_next"><?php echo t("Next highlight message"); ?></a>
+  <?php endif; ?>
 </div>
-
